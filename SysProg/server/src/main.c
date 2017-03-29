d376125aedae18fe9bdb39901f20378e4bb0a218
Luigi Venti <luigiventi@Luigis-MacBook-Pro.local>
2017-03-29 23:32:55 +0200

    Docker backbones

---
+++ SysProg/server/src/main.c
@@ -0,0 +1,10 @@
+//
+// Created by Luigi Venti on 29/03/17.
+//
+
+#include "main.h"
+#include <stdio.h>
+
+int main() {
+    printf("Hello world! Aridaje 2\n");
+}
---
+++ SysProg/server/src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Luigi Venti on 29/03/17.
+//
+
+#ifndef PROG_SISTEMA_2017_MAIN_H
+#define PROG_SISTEMA_2017_MAIN_H
+
+#endif //PROG_SISTEMA_2017_MAIN_H
